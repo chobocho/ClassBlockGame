fc8ad04ba99c099c5463a77ca21ea34679823725
ChengHe <chobocho1004@gmail.com>
2018-01-04 00:18:43 +0900

    Add code for unittest
    Add code for unittest

---
+++ src/ChoboTetrisActivity/app/src/test/java/com/chobocho/tetristest/TetrisTest.java
@@ -0,0 +1,31 @@
+package com.chobocho.tetristest;
+
+import com.chobocho.tetris.Tetris;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chobo on 2018-01-03.
+ */
+public class TetrisTest {
+    Tetris tetris;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetScore() {
+
+    }
+
+}
